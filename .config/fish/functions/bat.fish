ada9d11e9cf1a8191c1f0cb529c8303043fe3127
emil-lengman <emil.lengman@schibsted.com>
2023-02-03 14:39:38 +0100

    add fzf.fish

---
+++ .config/fish/functions/bat.fish
@@ -0,0 +1,4 @@
+function bat --wraps=/usr/bin/batcat --description 'alias bat=/usr/bin/batcat'
+  /usr/bin/batcat $argv
+        
+end
---
+++ .config/fish/functions/fd.fish
@@ -0,0 +1,4 @@
+function fd --wraps=/usr/bin/fdfind --description 'alias fd=/usr/bin/fdfind'
+  /usr/bin/fdfind $argv
+        
+end
