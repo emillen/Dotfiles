e475cc67245dc9929031528984bc352ad3751d31
emil-lengman <emil.lengman@schibsted.com>
2023-04-18 09:22:18 +0200

    change order of the cmp.config.sources

--- .config/nvim/lua/cmp-config.lua
+++ .config/nvim/lua/cmp-config.lua
@@ -50,8 +50,8 @@ cmp.setup({
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	},
 	sources = cmp.config.sources({
+        { name = "vsnip" }, -- For vsnip users.
 		{ name = "nvim_lsp" },
-		{ name = "vsnip" }, -- For vsnip users.
         { name = "buffer" },
 		-- { name = 'luasnip' }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
