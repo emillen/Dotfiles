7ae304d31489668e3b3713978dd8dc65bff798a6
emil-lengman <emil.lengman@schibsted.com>
2023-04-26 13:22:54 +0200

    add bindings for jumping some in the completion selection

--- .config/nvim/lua/cmp-config.lua
+++ .config/nvim/lua/cmp-config.lua
@@ -45,6 +45,22 @@ cmp.setup({
 			end
 		end, { "i", "s" }),
 
+		["<C-d>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_next_item({ count = 5 })
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-next)", "")
+			end
+		end, { "i", "s" }),
+
+		["<C-u>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item({ count = 5 })
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s" }),
+
 		["<C-Space>"] = cmp.mapping.complete(),
 
 		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
