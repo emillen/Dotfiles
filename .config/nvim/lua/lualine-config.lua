0a80f0d45189027e06065389a5bd90843c6af690
emil-lengman <emil.lengman@schibsted.com>
2023-04-26 13:23:30 +0200

    fix my own theme for lualine

--- .config/nvim/lua/lualine-config.lua
+++ .config/nvim/lua/lualine-config.lua
@@ -1,20 +1,46 @@
-local theme = require("lualine.themes.modus-vivendi")
-local orange = "#cc7832"
-local green = "#6a8759"
-local yellow = "#ffc66d"
-local purple = "#9876aa"
+local colors = {
+	orange = "#cc7832",
+	green = "#6a8759",
+	yellow = "#ffc66d",
+	purple = "#9876aa",
+	gray = "#323232",
+	black = "#2b2b2b",
+	white = "#c5c8c6",
+	blue = "#6897bb",
+}
 
-theme.normal.a.bg = orange
-theme.normal.b.fg = orange
-
-theme.insert.a.bg = green
-theme.insert.b.fg = green
-
-theme.visual.a.bg = purple
-theme.visual.b.fg = purple
-
-theme.replace.a.bg = yellow
-theme.replace.b.fg = yellow
+local theme = {
+	normal = {
+		a = { bg = colors.orange, fg = colors.black, gui = "bold" },
+		b = { bg = colors.gray, fg = colors.white },
+		c = { bg = colors.gray, fg = colors.white },
+	},
+	insert = {
+		a = { bg = colors.green, fg = colors.black, gui = "bold" },
+		b = { bg = colors.gray, fg = colors.white },
+		c = { bg = colors.gray, fg = colors.white },
+	},
+	visual = {
+		a = { bg = colors.purple, fg = colors.black, gui = "bold" },
+		b = { bg = colors.gray, fg = colors.white },
+		c = { bg = colors.gray, fg = colors.white },
+	},
+	replace = {
+		a = { bg = colors.yellow, fg = colors.black, gui = "bold" },
+		b = { bg = colors.gray, fg = colors.white },
+		c = { bg = colors.gray, fg = colors.white },
+	},
+	command = {
+		a = { bg = colors.blue, fg = colors.black, gui = "bold" },
+		b = { bg = colors.gray, fg = colors.white },
+		c = { bg = colors.gray, fg = colors.white },
+	},
+	inactive = {
+		a = { bg = colors.orange, fg = colors.black, gui = "bold" },
+		b = { bg = colors.gray, fg = colors.white },
+		c = { bg = colors.gray, fg = colors.white },
+	},
+}
 
 require("lualine").setup({
 	options = {
