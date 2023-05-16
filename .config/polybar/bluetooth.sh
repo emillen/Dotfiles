ad04bea0aca12bf785298cd55632397901a06bbb
emil-lengman <emil.lengman@schibsted.com>
2023-05-16 05:18:51 +0200

    some cleanup

--- .config/polybar/bluetooth.sh
+++ .config/polybar/bluetooth.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
 list_connected_devices() {
-    local connected_devieds=$(bluetoothctl paired-devices | awk '{print $2}')
-    local count=0
-    local max=$(echo "$connected_devieds" | wc -l)
-    for device in $connected_devieds; do
+    local connected_devices=$(bluetoothctl paired-devices | awk '{print $2}')
+    for device in $connected_devices; do
         local info=$(bluetoothctl info "$device")
 
         if echo "$info" | grep -q "Connected: yes"; then
             local alias=$(echo "$info" | grep "Alias" | cut -d ' ' -f 2-)
-            local count=$((count + 1))
-
-            echo -n "$alias"
-            if [ "$count" -lt "$max" ]; then
-                echo -n ", "
-            fi
+            echo "$alias"
         fi
     done
+}
+
+print_connected_devices() {
+
+    local connected_devices="$(list_connected_devices)"
+
+    local count=0
+    local max=$(echo "$connected_devices" | wc -l)
+
+    while read -r device; do
+        local count=$((count + 1))
+        echo -n "$device"
+        if [ "$count" -lt "$max" ]; then
+            echo -n ", "
+        fi
+    done < <(echo "$connected_devices")
 }
 
 main() {
@@ -30,7 +39,7 @@ List connected bluetooth devices.
 EOF
     )
     case "$1" in
-        --list-devices) list_connected_devices
+        --list-devices) print_connected_devices
             ;;
         *) echo "$help_text"
             ;;
@@ -38,4 +47,6 @@ EOF
 
 }
 
-main "$@"
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
