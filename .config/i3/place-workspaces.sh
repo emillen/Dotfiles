e9c2b983db65d2749f65c1fb95d23c3cb4953642
emil-lengman <emil.lengman@schibsted.com>
2023-05-20 23:32:33 +0200

    place the sscreens more like how i set them up manually

--- .config/i3/place-workspaces.sh
+++ .config/i3/place-workspaces.sh
@@ -13,13 +13,23 @@ get_displays() {
 place_workspaces () {
     local displays="$(get_displays)"
     local initial_workspace="$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name' )"
+    local current_workspace=1
 
-    local current=1
+    # fill all displays with workspaces
+    local workspaces_per_display=$((4 / ($(echo "$displays" | wc -l) - 1)))
     while read -r display; do
-        i3-msg "workspace $current; move workspace to output $display" > /dev/null
-        i3-msg "workspace $(($current + 1)); move workspace to output $display" > /dev/null
-        local current=$((current + 2))
+        for _ in $(seq 1 $workspaces_per_display); do
+            i3-msg "workspace $current_workspace; move workspace to output $display" > /dev/null
+            local current_workspace=$((current_workspace + 1))
+        done
     done < <(echo "$displays")
+
+    # fill out the last display with remaining workspaces
+    local last_display="$(echo "$displays" | tail -n 1)"
+    for _ in $(seq $current_workspace 9); do
+        i3-msg "workspace $current_workspace; move workspace to output $last_display" > /dev/null
+        local current_workspace=$((current_workspace + 1))
+    done
 
     i3-msg "workspace $initial_workspace" > /dev/null
 }
