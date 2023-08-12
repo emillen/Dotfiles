8482dd0b6e431038d89ea4a5e32a906961d184af
emillen <emil.lengman@gmail.com>
2023-08-12 09:37:01 +0200

    add ability to connect to vpn through eww

---
+++ .config/eww/scripts/connect-vpn.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+main() {
+    local nord_countries="$(nordvpn countries | sed -Ee "s/\s/\n/g" | tr -s "\n" | grep "\w" )$(echo -e '\nDisconnect')"
+
+    local country="$(echo "$nord_countries" | grep "\w" | fzf --tac)"
+
+    if [[ -z $country ]]; then
+        exit 0;
+    fi
+
+    if [[ $country == "Disconnect" ]]; then
+        nordvpn disconnect
+        exit 0;
+    fi
+
+    local city=$(nordvpn cities $country | sed -Ee "s/\s/\n/g" | tr -s "\n" | grep "\w"| fzf )
+
+    if [[ -z $city ]]; then
+        exit 0;
+    fi
+
+    nordvpn connect $country $city
+}
+
+if [[ "$BASH_SOURCE" == "$0" ]]; then
+    main
+fi
